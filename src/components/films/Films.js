724987b318e96df4874e234bb1f87a7dbed99cd6
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-09-26 18:52:46 +0300

    feat: connect to API  yts

--- src/components/films/Films.js
+++ src/components/films/Films.js
@@ -14,6 +14,26 @@ function Films (){
     </main>
         
     )
+class Films extends React.Component {
+  state ={
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async() => {
+    const {
+      data: {
+        data: {movies},
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
+    this.setState({movies, isLoading: false});
+  };
+
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
 }
 
 export default Films
