e3182c8d3c135ec9d963fda15d865224832b0c07
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-10-30 07:55:21 +0300

    feat: add useContext

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import AppContext from './Context';
 
 function App() {
   return ( 
+    <AppContext.Provider value={{
+      searchValue,
+      onChangeSearchInput, 
+      setSearchValue,
+      items,
+      setCurrentPage,
+      isLoading,
+      theme,
+      setTheme,
+      favorites,
+      addToFavorite,
+      isFavoriteAdded
+    }}>
     <div className="App">
       <Header/>
       <Main/>
       
     </div>
+    </AppContext.Provider>
   );
 }
 
