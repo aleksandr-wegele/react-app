134496eb09b0b34b63501f114c79eb3d8c740c84
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-10-30 08:03:13 +0300

    feat: search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,38 @@
-import Logo from './img/logo.png'
+import React from "react";
 import { Link } from "react-router-dom";
 
-function Header (){
+import AppContext from "../Context";
+
+function Header() {
+
+const {
+  searchValue, 
+  onChangeSearchInput, 
+  setSearchValue,
+  setTheme
+  } = React.useContext(AppContext);
+
+const toggleThemeLight = () =>{
+  setTheme('light')
+}
+const toggleThemeDark = () => {
+  setTheme('dark');
+}
+
     return(
         <header className="header">
         <div className="wrap__content_header">
-          <img src={Logo} alt="Logo" className="header__logo" />
           <Link to="/"><img src='/img/logo.png' alt="Logo" className="header__logo" /></Link>
           <div className="header__search_block">
-            <input className="header__search_input" placeholder="Search" />
+            <img src='/img/search.png' alt='search' className='search_img'/>
+            {searchValue && <img onClick={()=> setSearchValue('')} src='/img/close.svg' alt='clear_search' className='clear_search_btn'/>}
+            <input onChange={onChangeSearchInput} value={searchValue} className="header__search_input" placeholder="Search..." />
           </div>
-            <div className="swiper_theme">Dark theme</div>
+            <button onClick={toggleThemeDark}>Темная тема</button>
+            <button onClick={toggleThemeLight}>Светлая тема</button>
         </div>
       </header>
     )
 }
+
 export default Header
